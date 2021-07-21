48bfb75f6ee23514d74922819df8a4352027b2aa
MaevaWolff <maeva.wolff@hetic.net>
2021-07-21 20:03:46 +0200

    add cors config in app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const cors = require('cors')
 const connectDB = require('./config/db');
+
 
 const app = express();
 // Init Middleware
 app.use(express.json({ extended: false }));
+app.use(cors())
+
 
 // Connect DB
 connectDB();
